9a534bd86dc1d13f1df58131fbb3e1919b8c75f4
amalshehu <amalshehu@gmail.com>
2017-11-06 13:10:59 +0530

    Create onSubmit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Chat } from './app';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @Output() chat = new EventEmitter<Chat>();
   title = 'TDD';
   angularVersion = 5;
   chatForm = new FormGroup({
     user: new FormControl('', Validators.required),
-    message: new FormControl(''),
-    type: new FormControl('')
+    message: new FormControl('', Validators.required)
   });
+  constructor() {}
+  onSubmit() {
+    this.chat.emit({
+      user: this.chatForm.value.user,
+      message: this.chatForm.value.message,
+    });
+  }
 }
